230616b4da787338059c351b3f6277ff3d661f3a
bellaborrego38 <bellaborrego38@yahoo.com>
2024-01-26 13:54:21 -0600

    Created a main component(test) that successfully displays in the browser

---
+++ morgan-project/src/components/main.jsx
@@ -0,0 +1,12 @@
+import React,{ useState, useEffect } from 'react';
+
+function Main() {
+    return (
+        <div>
+            <h2> Hello World! </h2>
+            <p> What is going on </p>
+        </div>
+    );
+}
+
+export default Main;
